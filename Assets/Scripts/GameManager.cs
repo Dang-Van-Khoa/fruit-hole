57100ea13d68cc34e201c5fb689e8b4ef6806cf5
Khoadv <dangvankhoa15112001@gmail.com>
2025-12-03 21:34:06 +0700

    change unity version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextAsset lv;
+    [Button]
+    private void ReadLevelData()
+    {
+        TextAsset levelData = Resources.Load<TextAsset>("levelsencypted/levelsjson/Level1"); // bỏ .bytes
+        if (levelData == null)
+        {
+            Debug.LogError("Không tìm thấy file trong Resources");
+            return;
+        }
+
+        var data = levelData.bytes.ToString();
+
+        // In 20 byte đầu tiên
+        Debug.Log($"data level: {data}");
+    }
+}
